edadd0806fae7dd9993550d61bc628c50dfe1d8f
Legoglass <128423092+Legoglass@users.noreply.github.com>
2023-07-31 16:45:18 +0100

    Add files via upload

---
+++ meteo app/app.js
@@ -0,0 +1,34 @@
+
+//$(document).ready(function(){
+    //animateDiv('.circle');
+//});
+
+//function makeNewPosition(){
+    // Get viewport dimensions (remove the dimension of the div)
+    //var h = $(window).height() - 50;
+    //var w = $(window).width() - 50;
+    
+    //var nh = Math.floor(Math.random() * h);
+    //var nw = Math.floor(Math.random() * w);
+    
+    //return [nh,nw];   
+    
+//}
+
+//function animateDiv(myclass){
+   // var newq = makeNewPosition();
+   // $(myclass).animate({ top: newq[0], left: newq[1] }, 1000,   function(){
+      //animateDiv(myclass);        
+    //});
+    
+//};
+
+
+const c = document.getElementsByClassName(".conatiner");
+const ci = document.getElementsByClassName(".circle");
+
+const cN = c.offsetHeight;
+const cW = c.offsetWidth;
+
+console.log(cN);
+    
